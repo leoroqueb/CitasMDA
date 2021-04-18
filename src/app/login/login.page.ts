a259774468b95107ece9abf3a051c928035b756a
Juan José <diazsantanajuanjose@gmail.com>
2021-04-18 17:47:40 +0100

    Cambios en Login y Registro

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as $ from 'jquery';
-import { AuthService } from '../providers/auth.service'
+import { AuthService } from '../providers/auth.service';
 
 @Component({
   selector: 'app-login',
--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { AuthService } from '../providers/auth.service';
 import { Refactor } from '../refactor/refactor.service';
-
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-registration',
@@ -12,6 +12,21 @@ import { Refactor } from '../refactor/refactor.service';
 
 export class RegistrationPage implements OnInit {
 
+  public register = false;
+  public forms = {
+    form1: true,
+    form2: false,
+    form3: false
+  };
+  campos = {
+    email: '',
+    password: '',
+    genero: '',
+    direccion: '',
+    telefono: '',
+    dni: '',
+    ss: ''
+  };
   constructor(
     private router: Router,
     private refactor: Refactor,
@@ -19,9 +34,29 @@ export class RegistrationPage implements OnInit {
   ) { }
 
   ngOnInit(){}
-  
-  signUp(email, password){
+
+  signUp(){
+    console.log(this.campos);
     //this.authService.registerUser(email.value, password.value);
   }
 
+  next(){
+
+    // Del 2 al 3
+    if(this.forms.form2 === true){
+      this.forms.form2 = false;
+      this.forms.form3 = true;
+      $('#progressbar').val('1');
+      $('#subtitle').text('Y ya hemos terminado');
+      this.register = true;
+    }
+
+    // Del 1 al 2
+    if(this.forms.form1 === true){
+      this.forms.form1 = false;
+      this.forms.form2 = true;
+      $('#progressbar').val('0.5');
+      $('#subtitle').text('Un poco más...');
+    }
+  }
 }
