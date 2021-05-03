cd52f085ec56264817f6e4d3e3208c187c9647e6
Juan José <diazsantanajuanjose@gmail.com>
2021-05-03 19:30:22 +0100

    JJ: - Validators

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -6,6 +6,7 @@ import * as $ from 'jquery';
 import { UsuariosI } from '../models/users.model';
 import { UsuariosService } from '../providers/usuarios.service';
 import { CitasService } from '../providers/citas.service';
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-registration',
@@ -38,6 +39,7 @@ export class RegistrationPage implements OnInit {
     private userService: UsuariosService,
     private refactor: Refactor,
     private citasService: CitasService,
+    public toastController: ToastController
   ) { }
 
   ngOnInit(){}
@@ -52,20 +54,29 @@ export class RegistrationPage implements OnInit {
       nombre: this.campos.nombre,
       ss: this.campos.ss,
       telefono: this.campos.telefono
-    } 
+    };
 
-    
-    this.authService.registerUser(this.campos.email, this.campos.password)
-    .then(() => {
-      this.userService.registerData(userProfile)
-      .then(() =>{
-        this.refactor.presentToast("Cuenta creada correctamente");
-        this.router.navigateByUrl('home');
-      })
-      .catch((err) => this.refactor.presentToast("ERROR: " + err));
-      this.citasService.registerUserToAppointmentsBD(userProfile.dni);
-    })
-    .catch(err => this.refactor.presentToast("ERROR: " + err));
+    // Validamos los ultimos campos antes del registro
+    if(this.campos.genero === ''){
+      this.presentToast('Introduce tu género correctamente');
+    }else if(this.campos.direccion === ''){
+      this.presentToast('Introduce una dirección correcta');
+    }else if(this.campos.telefono === ''){
+      this.presentToast('Introduce un teléfono válido');
+    }else{
+      // Creamos el usuario
+      this.authService.registerUser(this.campos.email, this.campos.password)
+        .then(() => {
+          this.userService.registerData(userProfile)
+            .then(() =>{
+              this.refactor.presentToast("Cuenta creada correctamente");
+              this.router.navigateByUrl('home');
+            })
+            .catch((err) => this.refactor.presentToast("ERROR: " + err));
+          this.citasService.registerUserToAppointmentsBD(userProfile.dni);
+        })
+        .catch(err => this.refactor.presentToast("ERROR: " + err));
+    }
   }
 
   setGender(ev){
@@ -76,19 +87,74 @@ export class RegistrationPage implements OnInit {
 
     // Del 2 al 3
     if(this.forms.form2 === true){
-      this.forms.form2 = false;
-      this.forms.form3 = true;
-      $('#progressbar').val('1');
-      $('#subtitle').text('¡Listo!');
-      this.register = true;
+      // Comprobamos que no están vacios los campos
+      if(this.campos.dni === ''){
+        this.presentToast('Introduce tu DNI');
+      }else if(this.campos.nombre === ''){
+        this.presentToast('Introduce tu nombre');
+      }else if(this.campos.apellidos === ''){
+        this.presentToast('Introduce tus apellidos');
+      }else if(this.campos.ss === ''){
+        this.presentToast('Introduce tu número de la seguridad social');
+      }else{
+        this.forms.form2 = false;
+        this.forms.form3 = true;
+        $('#progressbar').val('1');
+        $('#subtitle').text('¡Listo!');
+        this.register = true;
+      }
     }
 
     // Del 1 al 2
     if(this.forms.form1 === true){
-      this.forms.form1 = false;
-      this.forms.form2 = true;
-      $('#progressbar').val('0.5');
-      $('#subtitle').text('Un poco más...');
+      // Comprobamos que no están vacios los campos
+      if(this.campos.email === ''){
+        this.presentToast('Introduce un correo electrónico válido');
+      }else if(this.campos.password === ''){
+          this.presentToast('Introduce una contraseña válida');
+      }else{
+        this.forms.form1 = false;
+        this.forms.form2 = true;
+        $('#progressbar').val('0.5');
+        $('#subtitle').text('Un poco más...');
+      }
     }
   }
+
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2500
+    });
+    toast.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    await toast.present();
+
+    const { role } = await toast.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 }
